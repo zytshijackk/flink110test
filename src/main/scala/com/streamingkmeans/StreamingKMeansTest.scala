ab057ca4c2a84d5273fe033cf26bfd836576a9b8
ChenHao <864171652@qq.com>
2020-04-28 16:08:40 +0800

    微批流式KMeans算法测试类

--- src/main/scala/com/streamingkmeans/StreamingKMeansTest.scala
+++ src/main/scala/com/streamingkmeans/StreamingKMeansTest.scala
@@ -1,7 +1,8 @@
 package com.streamingkmeans
 
-import com.streamingkmeans.utils.{StringToDense, StringToSparse}
+import com.streamingkmeans.utils.StringToDense
 import org.apache.flink.api.common.functions.{FlatMapFunction, RichMapFunction}
+import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.api.scala._
 import org.apache.flink.ml.math.{DenseVector, SparseVector}
 import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
@@ -10,45 +11,35 @@ import org.apache.flink.util.Collector
 object StreamingKMeansTest {
   def main(args: Array[String]): Unit = {
 
-    val input = "/Users/zytshijack/Documents/github/git/myrepositories/flink110test/src/main/resources/file/point.txt"
+//    val input = "/Users/zytshijack/Documents/github/git/myrepositories/flink110test/src/main/resources/file/point.txt"
+
+    // the port to connect to
+    var port = 0
+    try {
+      ParameterTool.fromArgs(args).getInt("port")
+    } catch {
+      case e: Exception => {
+        //System.err.println("No port specified. Please run 'StreamingKMeansTest --port <port>'")
+        port  = 9000
+      }
+    }
 
     // get the execution environment
     val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
 
-    val text:DataStream[String] = env.readTextFile(input)
-//    val list:DataStream[Array[Double]]  = text
-//      .flatMap(new FlatMapFunction[String,Array[Double]] {
-//        override def flatMap(t: String, collector: Collector[Array[Double]]): Unit = {
-//          val  word:Array[Double] = t.split("\\s").map(_.toDouble)
-//          collector.collect(word)
-//        }
-//      })
-
-//    val dense:DataStream[DenseVector] = text.map(new StringToDense)
-
-//      dense.countWindowAll(5)
-//      .apply{( window: GlobalWindow,
-//               events: SparseVector,
-//               out: Collector[(Double, Long)]) =>
-//        out.collect(update(events)))
-//      }
-
-//    dense.print()
-
-//    re.print()
-//    list.map(a=>)
-//    text.print()
-//    val win:AllWindowedStream[Array[String],TimeWindow]  = list.windowAll(TumblingProcessingTimeWindows.of(Time.seconds(5)))
-//    val win:AllWindowedStream[Array[String],GlobalWindow] = list.countWindowAll(5)
-//    val arr = Array(List(1,1),List(-1,-1),List(0,0))
+    // get input data by connecting to the socket
+    val text: DataStream[String] = env.socketTextStream("127.0.0.1", port, '\n')
     val arr1 = DenseVector(Array[Double](1,1))
     val arr2 = DenseVector(Array[Double](-1,-1))
     val arr = Array[DenseVector](arr1,arr2)
-    val skm = new StreamingKMeans(2,2,1,arr,Array(3.0,3.0))
-
-    skm.trainOnStream(text)
-
+    val skm = new StreamingKMeans2(2,2,0.6,arr,Array(3.0,2.0))
+    skm.train(text)
     env.execute()
+    val center = skm.clusterCenters
+    val weight = skm.clusterWeights
+    System.out.print("~~~~"+weight(1))
+    System.out.print("~~~~"+center(1))
+
   }
 //  case class VectorTest(var list: Array[String]) extends Serializable{
 //      var list: Array[Double]
