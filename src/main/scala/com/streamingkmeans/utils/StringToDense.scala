af9483e588e7422d5fd4c11f9d47409ba2ad825d
ChenHao <864171652@qq.com>
2020-05-11 11:34:16 +0800

    1

--- src/main/scala/com/streamingkmeans/utils/StringToDense.scala
+++ src/main/scala/com/streamingkmeans/utils/StringToDense.scala
@@ -7,7 +7,7 @@ import org.apache.flink.ml.math.{DenseVector, SparseVector}
 class StringToDense extends MapFunction[String, DenseVector] with Serializable {
 
   override def map(s: String): DenseVector = {
-    val arr:Array[Double] = s.split(",").map(x => x.toDouble)
+    val arr:Array[Double] = s.split(" ").map(x => x.toDouble)
     DenseVector(arr)
   }
 }
